66387cfb39faab62a1c08cca3c3084947cf3024b
Vadim Vikhrachev <vikhrachev@gmail.com>
2025-05-05 16:25:17 +0300

    fix url

--- main.py
+++ main.py
@@ -23,4 +23,4 @@ def playability(weather: badminton_model.Weather):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
